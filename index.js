e3cb96b81af395cfa8d65e4a91277b8e9337eb10
David Tewkesbury <tewkesburyd@hotmail.com>
2022-10-31 21:50:59 -0600

    commit tree 97fccff0033e73bea42eb7d5eb5c021854dc4b7c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (namesArray) => [namesArray[0], namesArray[1]];
+
+const returnLastTwoDrivers = (array) => [array[array.length-2], array[array.length-1]];
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (int) => function(){
+    const fare = int * int;
+    return fare;
+}
+
+const fareDoubler = (int) => {
+    const fare = parseInt(int) * 2;
+    return fare;
+}
+
+
+const fareTripler = (int) => {
+    const fare = parseInt(int) * 3;
+    return fare;
+}
+
+function selectDifferentDrivers(array , firstOrLast ){
+    if (firstOrLast === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(array);
+    } else if (firstOrLast === returnLastTwoDrivers){
+        return returnLastTwoDrivers(array);
+    };
+};
